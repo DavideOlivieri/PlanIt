a5cb88d38a5bbc3711a6c344c07bcd72a5101709
JacopoColoccioni <94982047+JacopoColoccioni@users.noreply.github.com>
2023-03-30 10:15:25 +0200

    creazione deell'azione che permette di passare dalla pagina accedi alla pagina registrati

--- app/src/main/java/com/example/planit/MainActivity.kt
+++ app/src/main/java/com/example/planit/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.planit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.datepicker.MaterialCalendar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_in)
+        val noAccount = findViewById<TextView>(R.id.sign_up_view)
 
+        noAccount.setOnClickListener{
+            val intent =  Intent(this,SignUp::class.java)
+            startActivity(intent)
+    }
     }
 
 }
