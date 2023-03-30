662c77a914301abe801eb7802c374524d2e9b126
JacopoColoccioni <94982047+JacopoColoccioni@users.noreply.github.com>
2023-03-30 13:20:08 +0200

    tolta la actioBar e aggiunta la possibilita di passare dalla activity di signUp a quella di login

--- app/src/main/java/com/example/planit/SignUp.kt
+++ app/src/main/java/com/example/planit/SignUp.kt
@@ -1,11 +1,19 @@
 package com.example.planit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        val siAccount = findViewById<TextView>(R.id.login_view)
+
+        siAccount.setOnClickListener{
+            val intent =  Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
