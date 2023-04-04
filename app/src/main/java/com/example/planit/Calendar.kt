3b4c23851519316d154f1d72137bea89eeefff57
DavideOlivieri <83114199+davi0118@users.noreply.github.com>
2023-04-04 18:20:09 +0200

    Calendar view

---
+++ app/src/main/java/com/example/planit/Calendar.kt
@@ -0,0 +1,14 @@
+package com.example.planit
+
+import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+
+class Calendar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_calendario)
+        val menu = setSupportActionBar(findViewById<Toolbar>(R.id.actionbar))
+
+    }
+}
