fb32a90af5ee22959699e2d9138f038cf3f0b3b2
DavideOlivieri <83114199+davi0118@users.noreply.github.com>
2023-04-28 15:45:15 +0200

    restore

--- app/src/main/java/com/example/planit/Home.kt
+++ app/src/main/java/com/example/planit/Home.kt
@@ -2,12 +2,17 @@ package com.example.planit
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.VIEW_MODEL_STORE_OWNER_KEY
 import com.example.calendario.R
 
 class Home : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -16,6 +21,7 @@ class Home : AppCompatActivity() {
         val account = findViewById<ImageView>(R.id.Account)
         val btnAdd = findViewById<Button>(R.id.Aggiungi_Calendario)
         val username = intent.getStringExtra("Username")
+
 
         // passaggio alla schermata di ModAccount
         account.setOnClickListener {
@@ -30,5 +36,26 @@ class Home : AppCompatActivity() {
             intent.putExtra("Username", username)
             startActivity(intent)
         }
+
+/*
+        // Aggiungi nuovi calendari
+        btnAdd.setOnClickListener {
+          fun onClick(view: View){
+              addButton()
+          }
+        }
+*/
+
     }
+
+/*
+    lateinit var button: Button
+    fun addButton(){
+        val constraint = findViewById<ConstraintLayout>(R.id.layout1)
+        button = Button(this)
+        button.setText("Calendario_1")
+        button.
+        constraint.addView(button)
+    }
+*/
 }
